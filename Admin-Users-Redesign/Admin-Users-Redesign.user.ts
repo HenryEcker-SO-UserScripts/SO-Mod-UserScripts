b2327e2f89c36960220ddb1d5bd341168b0a7dd5
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-01-22 17:24:19 -0500

    Managed tab switching (less full page refreshes)

--- Admin-Users-Redesign/Admin-Users-Redesign.user.ts
+++ Admin-Users-Redesign/Admin-Users-Redesign.user.ts
@@ -3,7 +3,7 @@
 // @description  Makes /admin/users a bit less busy
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.5
+// @version      0.0.6
 // @downloadURL  https://github.com/HenryEcker/SO-Mod-UserScripts/raw/master/Admin-Users-Redesign/Admin-Users-Redesign.user.js
 // @updateURL    https://github.com/HenryEcker/SO-Mod-UserScripts/raw/master/Admin-Users-Redesign/Admin-Users-Redesign.user.js
 //
@@ -106,136 +106,164 @@
         return url;
     }
 
-    // Build HTML for the page
-    function rebuildPage(currentTab: string, currentPage: string, displayName: string): JQuery {
-        return $('<div class="d-flex mb48"></div>')
-            .append(buildNav(currentTab))
-            .append(buildMainBody(currentTab, currentPage, displayName));
-    }
+    class AdminUsersPage {
+        private readonly mountPoint: JQuery;
+        private currentTab: string;
+        private currentPage: string;
+        private readonly displayName: string;
 
-
-    // Build Nav (Sidebar)
-    function buildNav(currentTab: string): JQuery {
-        return $('<nav class="flex--item fl-shrink0 mr32 wmn1 md:d-none" role="navigation"></nav>')
-            .append(buildNavUl(currentTab));
-    }
-
-    function buildNavUl(currentTab: string): JQuery {
-        const ul = $('<ul class="ps-sticky t64 s-navigation s-navigation__muted s-navigation__vertical"></ul>');
-        for (const [queryLocation, {tabNavName, tabTitle, mainOnly}] of Object.entries(config.tabInfo)) {
-            ul.append(buildNavLi(tabNavName, tabTitle, queryLocation, currentTab, mainOnly));
+        constructor(mountPoint: JQuery) {
+            this.mountPoint = mountPoint;
+            this.updatePageInformation();
+            this.displayName = $('.s-topbar--item.s-user-card .s-avatar').first().attr('title') as string;
+            this.attachOnPopStateTasks();
         }
-        return ul;
-    }
 
-    function buildNavLi(tabText: string, tabTitle: string, queryLocation: string, currentTab: string, mainOnly: boolean | undefined): JQuery | null {
-        if (mainOnly === true && StackExchange.options.site.isChildMeta) {
-            return null;
+        private updatePageInformation() {
+            const usp = new URLSearchParams(window.location.search);
+            this.currentTab = usp.get('tab') ?? config.defaultTab[StackExchange.options.site.isChildMeta ? 'meta' : 'main'];
+            this.currentPage = usp.get('page') ?? '1';
         }
-        return $(`<li><a class="s-navigation--item pr48 ps-relative${currentTab === queryLocation ? ' is-selected' : ''}" href="${buildURL(config.route, '', {tab: queryLocation}).toString()}" title="${tabTitle}">${tabText}</a></li>`);
-    }
 
-    // Build main container for data
-    function buildMainBody(currentTab: string, currentPage: string, displayName: string): JQuery {
-        const {tabTitle, dataLoadFromUrl, urlSearchParams} = config.tabInfo[currentTab];
+        private updateURLSearchParamPage() {
+            const newLocation = buildURL(config.route, window.location.search, {
+                tab: this.currentTab,
+                page: this.currentPage === '1' ? undefined : this.currentPage
+            }).toString();
+            history.pushState(null, '', newLocation);
+        }
 
-        const jsAutoLoadDiv = $(`<div class="js-auto-load" data-load-from="${buildURL(dataLoadFromUrl, urlSearchParams, {page: currentPage}).toString()}" aria-live="polite">${config.loadingComponent}</div>`);
-        attachLoadListenerToDiv(jsAutoLoadDiv[0], currentTab, displayName);
+        // Allow back and forward navigation to update page values
+        private attachOnPopStateTasks() {
+            window.addEventListener('popstate', (ev) => {
+                ev.preventDefault();
+                this.mountPoint.html(config.loadingComponent); // Replace with loading component because it's more confusing to not show any indication something's happening
+                this.updatePageInformation();
+                this.render(true);
+            });
+        }
 
-        return $(`<div id="${config.bodyId}"></div>`)
-            .append(`<h2>${tabTitle}</h2>`)
-            .append(jsAutoLoadDiv);
-    }
+        render(manualLoad = false) {
+            this.mountPoint
+                .empty()
+                .append(this.rebuildPage(manualLoad));
+        }
 
-    // Attach mutation observer to monitor for when the DOM elements have been added
-    function attachLoadListenerToDiv(node: HTMLElement, currentTab: string, displayName: string) {
-        const dataObserver = new MutationObserver((mutationList, observer) => {
-            for (const mutation of mutationList) {
-                if (
-                    mutation.type === 'childList' &&
-                    mutation.addedNodes.length === 5 && // This is a set number of elements
-                    (mutation.target as HTMLElement).classList.contains('js-auto-load-target')
-                ) {
-                    addListenerToPaginationItems();
-                    if (config.tabInfo[currentTab].highlightSelf) {
-                        highlightOwnItems(displayName);
+        private rebuildPage(manualLoad: boolean): JQuery {
+            return $('<div class="d-flex mb48"></div>')
+                .append(this.buildNav())
+                .append(this.buildMainBody(manualLoad));
+        }
+
+        // Build Nav (Sidebar)
+        buildNav(): JQuery {
+            return $('<nav class="flex--item fl-shrink0 mr32 wmn1 md:d-none" role="navigation"></nav>')
+                .append(this.buildNavUl());
+        }
+
+        buildNavUl(): JQuery {
+            const ul = $('<ul class="ps-sticky t64 s-navigation s-navigation__muted s-navigation__vertical"></ul>');
+            for (const [queryLocation, {tabNavName, tabTitle, mainOnly}] of Object.entries(config.tabInfo)) {
+                ul.append(this.buildNavLi(tabNavName, tabTitle, queryLocation, mainOnly));
+            }
+            return ul;
+        }
+
+        buildNavLi(tabText: string, tabTitle: string, queryLocation: string, mainOnly: boolean | undefined): JQuery | null {
+            if (mainOnly === true && StackExchange.options.site.isChildMeta) {
+                return null;
+            }
+            return $('<li></li>').append(this.buildNavAnchor(tabText, tabTitle, queryLocation));
+        }
+
+        buildNavAnchor(tabText: string, tabTitle: string, queryLocation: string) {
+            const href = buildURL(config.route, '', {tab: queryLocation}).toString();
+            const a = $(`<a class="s-navigation--item pr48 ps-relative${this.currentTab === queryLocation ? ' is-selected' : ''}" href="${href}" title="${tabTitle}">${tabText}</a>`);
+            a.on('click', (ev) => {
+                ev.preventDefault();
+                this.currentPage = '1';
+                this.currentTab = queryLocation;
+                this.updateURLSearchParamPage();
+                this.render(true);
+            });
+            return a;
+        }
+
+
+        // Build main container for data
+        private buildMainBody(manualLoad: boolean): JQuery {
+            const {tabTitle, dataLoadFromUrl, urlSearchParams} = config.tabInfo[this.currentTab];
+
+            const loadFrom = buildURL(dataLoadFromUrl, urlSearchParams, {page: this.currentPage}).toString();
+            const dataBody = $(`<div class="js-auto-load" data-load-from="${loadFrom}" aria-live="polite">${config.loadingComponent}</div>`);
+            this.attachLoadListenerToDiv(dataBody[0]);
+
+            if (manualLoad) {
+                dataBody
+                    .load(loadFrom)
+                    .removeClass('js-auto-load')
+                    // Important! attachLoadListenerToDiv (and stock SE code) relies on the 'js-auto-load-target' class to attach listeners and perform pagination
+                    .addClass('js-auto-load-target');
+            }
+
+            return $(`<div id="${config.bodyId}"></div>`)
+                .append(`<h2>${tabTitle}</h2>`)
+                .append(dataBody);
+        }
+
+        // Attach mutation observer to monitor for when the DOM elements have been added
+        private attachLoadListenerToDiv(node: HTMLElement) {
+            const dataObserver = new MutationObserver((mutationList, observer) => {
+                for (const mutation of mutationList) {
+                    if (mutation.type === 'childList' &&
+                        mutation.addedNodes.length === 5 && // This is a set number of elements
+                        (mutation.target as HTMLElement).classList.contains('js-auto-load-target')
+                    ) {
+                        this.addListenerToPaginationItems();
+                        if (config.tabInfo[this.currentTab].highlightSelf) {
+                            this.highlightOwnItems();
+                        }
+                        break; // We've found what we need don't look through any more mutations
                     }
-                    break; // We've found what we need don't look through any more mutations
                 }
-            }
-            observer.disconnect(); // Don't care about any future DOM updates
-        });
-
-        const observerConfig = {childList: true};
-
-        dataObserver.observe(node, observerConfig);
-        // Re-attach observer on next ajax call
-        $(document).on('ajaxSend', (_0, _1, {url}) => {
-            if (url.startsWith(config.route)) {
-                dataObserver.observe(node, observerConfig);
-            }
-        });
-    }
-
-
-    function addListenerToPaginationItems() {
-        $('.js-ajax .s-pagination--item').on('click', (ev) => {
-            updateURLSearchParamPage(
-                new URLSearchParams(
-                    buildURL((ev.target as HTMLAnchorElement).href).search
-                ).get('page')
-            );
-        });
-    }
-
-    function updateURLSearchParamPage(newPage: string | null | undefined) {
-        if (newPage === null || newPage === undefined) {
-            return;
+                observer.disconnect(); // Don't care about any future DOM updates
+            });
+            const observerConfig = {childList: true};
+            dataObserver.observe(node, observerConfig);
+            // Re-attach observer on next ajax call
+            $(document).on('ajaxSend', (_0, _1, {url}) => {
+                if (url.startsWith(config.route)) {
+                    dataObserver.observe(node, observerConfig);
+                }
+            });
         }
-        const newLocation = buildURL(config.route, window.location.search, {'page': newPage === '1' ? undefined : newPage}).toString();
-        history.pushState(null, '', newLocation);
+
+        private addListenerToPaginationItems() {
+            $('.js-ajax .s-pagination--item').on('click', (ev) => {
+                this.currentPage = new URLSearchParams(
+                    buildURL((ev.target as HTMLAnchorElement).href).search
+                ).get('page');
+                this.updateURLSearchParamPage();
+            });
+        }
+
+        // Highlight items that contain your display name as the author
+        private highlightOwnItems() {
+            $(`.annotime:contains("${this.displayName}")`)
+                .closest('tr')
+                .addClass(config.selfActionClass);
+        }
     }
 
-    // Highlight items that contain your display name as the author
-    function highlightOwnItems(displayName: string) {
-        $(`.annotime:contains("${displayName}")`)
-            .closest('tr')
-            .addClass(config.selfActionClass);
-    }
-
-    // Allow back and forward navigation to update page values
-    function attachOnPopStateTasks() {
-        window.addEventListener('popstate', (ev) => {
-            ev.preventDefault();
-            const {currentTab, currentPage} = fetchInformationFromPage();
-            const {dataLoadFromUrl, urlSearchParams} = config.tabInfo[currentTab];
-            $(`#${config.bodyId} .js-auto-load-target`)
-                .html(config.loadingComponent) // Replace with loading component because it's more confusing to not show any indication something's happening
-                .load(buildURL(dataLoadFromUrl, urlSearchParams, {'page': currentPage}).toString());
-        });
-    }
-
-    // Helper to get various information from page/URL
-    function fetchInformationFromPage(): {
-        currentTab: string;
-        currentPage: string;
-        displayName: string;
-    } {
-        const usp = new URLSearchParams(window.location.search);
-        return {
-            currentTab: usp.get('tab') ?? config.defaultTab[StackExchange.options.site.isChildMeta ? 'meta' : 'main'],
-            currentPage: usp.get('page') ?? '1',
-            displayName: $('.s-topbar--item.s-user-card .s-avatar').first().attr('title') as string
-        };
-    }
 
     function main() {
         const mountPoint = $('.content-page');
         mountPoint.empty();
+
+        const newPage = new AdminUsersPage(mountPoint);
+
         StackExchange.ready(() => {
-            const {currentTab, currentPage, displayName} = fetchInformationFromPage();
-            mountPoint.append(rebuildPage(currentTab, currentPage, displayName));
-            attachOnPopStateTasks();
+            newPage.render();
         });
     }
 
