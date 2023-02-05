61e1828fbd598ef7aa2e2713dd164b9bccf636c7
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-02-05 13:21:06 -0500

    rebuild; bump version; update description

--- BanEvasionAccountDeleteHelper/vite.config.js
+++ BanEvasionAccountDeleteHelper/vite.config.js
@@ -4,10 +4,10 @@ export default buildViteConfig(
     'BanEvasionAccountDeleteHelper',
     `// ==UserScript==
 // @name         Ban Evasion Account Delete Helper
-// @description  Adds streamlined interface to deleting, annotating, and messaging accounts
+// @description  Adds streamlined interface for deleting evasion accounts, then annotating and messaging the main accounts
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.3
+// @version      0.0.4
 // @downloadURL  https://github.com/HenryEcker/SO-Mod-UserScripts/raw/master/BanEvasionAccountDeleteHelper/dist/BanEvasionAccountDeleteHelper.user.js
 // @updateURL    https://github.com/HenryEcker/SO-Mod-UserScripts/raw/master/BanEvasionAccountDeleteHelper/dist/BanEvasionAccountDeleteHelper.user.js
 //
