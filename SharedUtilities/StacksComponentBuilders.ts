0bd428bdf332ad88013e9527634628ad3e2b2ad0
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-02-05 13:25:52 -0500

    Support .prop assignment

--- SharedUtilities/StacksComponentBuilders.ts
+++ SharedUtilities/StacksComponentBuilders.ts
@@ -1,10 +1,12 @@
 // JQuery Component Builders
-export function attachAttrs<T>(e: JQuery<T>, attrs: Record<string, unknown>): JQuery<T> {
+export function attachAttributes<T>(e: JQuery<T>, attrs: Record<string, unknown>): JQuery<T> {
     for (const [key, value] of Object.entries(attrs)) {
         if (key === 'className') {
             e.addClass(value as string);
         } else if (key === 'htmlFor') {
             e.attr('for', value as string);
+        } else if (value === true || value === false) {
+            e.prop(key, value);
         } else {
             e.attr(key, value as string);
         }
@@ -14,21 +16,21 @@ export function attachAttrs<T>(e: JQuery<T>, attrs: Record<string, unknown>): JQ
 
 
 export function buildLabel(text: string, attrs?: Record<string, unknown>): JQuery<HTMLLabelElement> {
-    return attachAttrs(
+    return attachAttributes(
         $(`<label class="s-label">${text}</label>`),
         attrs ?? {}
     );
 }
 
 export function buildInput(attrs: Record<string, unknown>): JQuery<HTMLInputElement> {
-    return attachAttrs(
+    return attachAttributes(
         $('<input class="s-input"/>'),
         attrs
     );
 }
 
 export function buildButton(text: string, attrs?: Record<string, unknown>): JQuery<HTMLButtonElement> {
-    return attachAttrs(
+    return attachAttributes(
         $(`<button class="s-btn">${text}</button>`),
         attrs ?? {}
     );
