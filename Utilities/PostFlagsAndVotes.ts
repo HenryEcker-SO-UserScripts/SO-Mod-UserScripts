f2e4fcbf54fae8850abeff67616f8b3e2b010bc0
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-02-11 22:17:05 -0500

    Continue adding action util functions

--- Utilities/PostFlagsAndVotes.ts
+++ Utilities/PostFlagsAndVotes.ts
@@ -6,3 +6,10 @@ export function castPostsVote(postId: number | string, voteType: number | string
         body: getFormDataFromObject({fkey: StackExchange.options.user.fkey})
     });
 }
+
+export function reopenQuestion(postId: number | string) {
+    return fetch(`/flags/questions/${postId}/reopen/add`, {
+        method: 'POST',
+        body: getFormDataFromObject({fkey: StackExchange.options.user.fkey})
+    });
+}
