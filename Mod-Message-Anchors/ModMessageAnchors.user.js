56bf1723c52f2a3a427ee2365f233f46fefe4d62
HenryEcker <hde1997@gmail.com>
2023-03-07 00:25:08 -0500

    Improved link location/style

--- Mod-Message-Anchors/ModMessageAnchors.user.js
+++ Mod-Message-Anchors/ModMessageAnchors.user.js
@@ -3,7 +3,7 @@
 // @description  Adds anchor links to specific mod messages and CM escalation messages
 // @homepage     https://github.com/HenryEcker/SO-Mod-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.1
+// @version      0.0.2
 // @downloadURL  https://github.com/HenryEcker/SO-Mod-UserScripts/raw/master/Mod-Message-Anchors/ModMessageAnchors.user.js
 // @updateURL    https://github.com/HenryEcker/SO-Mod-UserScripts/raw/master/Mod-Message-Anchors/ModMessageAnchors.user.js
 //
@@ -34,10 +34,12 @@
 /* globals $ */
 (function () {
     $('.js-msg-body,.msg-body')
-        .each((i, message) => {
-            const anchor = $(message).find('a[name]'); // assumes this is the only anchor with a name attribute
+        .each((i, n) => {
+            const message = $(n);
+            const anchor = message.find('a[name]').first(); // assumes this is the only anchor with a name attribute
             const name = anchor.attr('name');
-            anchor.attr('href', `#${name}`);
-            anchor.text(name);
+            message
+                .find('div:eq(0)')
+                .append(`<span class="d-block w100"><a class="my2" href="#${name}">${name}</a></span>`);
         });
 })();
