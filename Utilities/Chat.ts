9d2c54b1284b607c2754a63868349f086e5c14b6
HenryEcker <hde1997@gmail.com>
2023-03-07 18:54:36 -0500

    Add chat delete message function

--- Utilities/Chat.ts
+++ Utilities/Chat.ts
@@ -2,9 +2,18 @@ import {type IdType} from './Types';
 import {fetchPostFormData} from './General';
 
 
-export function sendChatMessage(roomId: IdType, messageText: string, fkey: string) {
+export function sendChatMessage(roomId: IdType, messageText: string, chatFkey: string) {
+    // Will respond with a 200 if successful with JSON {id: number; time: number;}
+    // Will respond with a 409 if rate limited. The response message is plaintext not JSON
     return fetchPostFormData(`/chats/${roomId}/messages/new`, {
         'text': messageText,
-        'fkey': fkey
+        'fkey': chatFkey
+    });
+}
+
+export function deleteChatMessage(messageId: IdType, chatFkey: string) {
+    // Will respond with a 200 if successful and text "ok"
+    return fetchPostFormData(`/messages/${messageId}/delete`, {
+        'fkey': chatFkey
     });
 }
