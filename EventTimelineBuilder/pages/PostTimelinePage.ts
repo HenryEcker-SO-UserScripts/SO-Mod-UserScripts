58b99dbe8a246734c18faa20e427e52606fdcd38
HenryEcker <hde1997@gmail.com>
2023-03-10 00:30:22 -0500

    Note

--- EventTimelineBuilder/pages/PostTimelinePage.ts
+++ EventTimelineBuilder/pages/PostTimelinePage.ts
@@ -1,6 +1,16 @@
 import {addControllerAttributes} from '../Utils';
 
 
+/*
+TODO:
+Musing... it might be much better to not store all of this for every timestamp, but rather simply add
+a dedicated controller function for each page time e.g.
+ click->CONTROLLER#handlePostTimelineTimestampClick, click->CONTROLLER#handleReputationEventClick, etc.
+
+That was less information needs to be calculated and store in the DOM
+and we only parse events when we need the information.
+ */
+
 function parseTr(tr: JQuery) {
     const typeOfEvent = tr.find('td:eq(1)').text().trim();
     const linkToEvent = tr.find('td:eq(2) a').attr('href');
