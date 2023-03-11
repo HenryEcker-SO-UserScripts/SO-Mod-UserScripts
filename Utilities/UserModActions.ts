0c930d61833c7afd2eb3cd5cc6d10c7b74fe5aa8
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-03-11 00:16:40 -0500

    Move buildDetailStringFromObject function to General (prevents duplication on testing)

--- Utilities/UserModActions.ts
+++ Utilities/UserModActions.ts
@@ -21,37 +21,6 @@ export function getUserPii(userId: IdType): Promise<{
         });
 }
 
-/**
- * Creates formatted detail string with columns for delete/annotation detail text
- */
-export function buildDetailStringFromObject(obj: Record<string, string>, keyValueSeparator: string, recordSeparator: string, alignColumns = false) {
-    const filteredObj = Object.entries(obj)
-        .reduce((acc, [key, value]) => {
-            if (value.length > 0) {
-                acc[`${key}${keyValueSeparator}`] = value;
-            }
-            return acc;
-        }, {} as Record<string, string>);
-
-    const getPaddingStr = (function () {
-        if (alignColumns) {
-            const maxLabelLength = Math.max(...Object.keys(filteredObj).map(k => k.length));
-            return function (key: string) {
-                return new Array(maxLabelLength - key.length + 1).join(' ');
-            };
-        } else {
-            return function (_: unknown) {
-                return '';
-            };
-        }
-    }());
-
-    return Object.entries(filteredObj)
-        .map(([key, value]) => `${key}${getPaddingStr(key)}${value}`)
-        .join(recordSeparator);
-
-}
-
 export type DeleteReason = (
     'This user was created to circumvent system or moderator imposed restrictions and continues to contribute poorly' |
     'This user is no longer welcome to participate on the site'
