6bc4ce0c3ce16829a2e3a6c42897af36954789c5
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-03-13 00:58:45 -0400

    Pack and export stimulus html components directly

--- BanEvasionAccountDeleteHelper/vite.config.js
+++ BanEvasionAccountDeleteHelper/vite.config.js
@@ -1,14 +1,6 @@
 import buildViteConfig from '../vite-config-builder';
 import filterReplace from 'vite-plugin-filter-replace';
-import {
-    actions,
-    controllerTargets,
-    dataController,
-    initialModal,
-    modalId,
-    remainingFormFields,
-    targets
-} from './util-beadh/stimulus-html-components';
+import stimulusHtmlDefineObj from './util-beadh/stimulus-html-components';
 import beautifyPlugin from '../vite-plugin-beautify-output';
 import {buildMatchPatterns} from '../banner-build-util';
 
@@ -62,25 +54,7 @@ export default ({mode}) => {
     }
 
     config.define = {
-        // HTML
-        JS_MODAL_ID: JSON.stringify(modalId),
-        INITIAL_MODAL_HTML: `\`${initialModal}\``,
-        MODAL_FORM_HTML: `\`${remainingFormFields}\``,
-        // STIMULUS DATA CONTROLLER STRINGS
-        DATA_CONTROLLER: JSON.stringify(dataController),
-        MAIN_ACCOUNT_ID_INPUT_TARGET: JSON.stringify(targets.mainAccountIdInput),
-        MAIN_ACCOUNT_ID_INPUT_BUTTON_TARGET: JSON.stringify(targets.mainAccountIdInputButton),
-        FORM_ELEMENTS_TARGET: JSON.stringify(targets.formElements),
-        DELETION_REASON_SELECT_TARGET: JSON.stringify(targets.deletionReasonSelect),
-        DELETION_DETAILS_TARGET: JSON.stringify(targets.deletionDetails),
-        ANNOTATION_DETAILS_TARGET: JSON.stringify(targets.annotationDetails),
-        SHOULD_MESSAGE_AFTER_TARGET: JSON.stringify(targets.shouldMessageAfter),
-        CONTROLLER_SUBMIT_BUTTON_TARGET: JSON.stringify(targets.controllerSubmitButton),
-        CONTROLLER_TARGETS: JSON.stringify(controllerTargets), // <- string[] not string!!
-        // String Replacement of function name (not in vite-define.d)
-        HANDLE_SUBMIT_ACTION: actions.handleSubmitActions,
-        HANDLE_CANCEL_ACTION: actions.handleCancelActions,
-        HANDLE_LOOKUP_MAIN_ACCOUNT: actions.handleLookupMainAccount,
+        ...stimulusHtmlDefineObj
     };
     return config;
 };
