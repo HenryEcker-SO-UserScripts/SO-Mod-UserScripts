18aa70f92260333515010451cb621e2d19384b92
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-03-24 22:00:08 -0400

    Move utility functions to separate repo; update imports and rebuild

--- Utilities/Types.ts
+++
@@ -1,8 +0,0 @@
-export type IdType = number | string;
-
-export interface BaseStacksControllerConfig {
-    targets?: string[];
-    initialize?: () => void;
-    connect?: () => void;
-    disconnect?: () => void;
-}
