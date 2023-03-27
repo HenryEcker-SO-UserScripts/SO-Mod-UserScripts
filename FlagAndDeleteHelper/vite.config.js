cfee119b4ce20f01de25c034150535e15a28af13
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-03-27 15:36:37 -0400

    0.0.5

--- FlagAndDeleteHelper/vite.config.js
+++ FlagAndDeleteHelper/vite.config.js
@@ -11,7 +11,7 @@ const banner = `// ==UserScript==
 // @description  Adds a "${postButtonLabel}" button to all posts that assists in raising text flags and immediately handling them
 // @homepage     https://github.com/HenryEcker/SO-Mod-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.4
+// @version      0.0.5
 // @downloadURL  https://github.com/HenryEcker/SO-Mod-UserScripts/raw/master/FlagAndDeleteHelper/dist/FlagAndDeleteHelper.user.js
 // @updateURL    https://github.com/HenryEcker/SO-Mod-UserScripts/raw/master/FlagAndDeleteHelper/dist/FlagAndDeleteHelper.user.js
 //
