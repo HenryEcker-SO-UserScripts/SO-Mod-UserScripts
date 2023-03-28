aca287bd866fff39feb6709dc09c04f463f321b1
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-03-27 22:50:17 -0400

    Remove and place in own repo

--- FlagAndDeleteHelper/Globals.ts
+++
@@ -1,7 +0,0 @@
-export function getModalId(postId: number) {
-    return FADHNS.JS_MODAL_ID.formatUnicorn({
-        postId: postId
-    });
-}
-
-export type ModFlagRadioType = 'mod-flag' | 'plagiarism';
