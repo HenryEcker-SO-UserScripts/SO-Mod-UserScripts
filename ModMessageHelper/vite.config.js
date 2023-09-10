689a22c5907f574d24c6e43768fe3ba2e8cdbd7f
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-09-09 22:06:54 -0400

    Mod Message Helper Initial Commit

---
+++ ModMessageHelper/vite.config.js
@@ -0,0 +1,21 @@
+import buildViteConfig from '../vite-config-builder';
+import {buildMatchPatterns} from '../banner-build-util';
+
+
+export default buildViteConfig(
+    'ModMessageHelper',
+    `// ==UserScript==
+// @name         Custom Mod Message Templates V2
+// @description  Adds mod message templates with default configurations to the mod message drop-down
+// @homepage     https://github.com/HenryEcker/SO-Mod-UserScripts
+// @author       Henry Ecker (https://github.com/HenryEcker)
+// @version      0.0.1
+// @downloadURL  https://github.com/HenryEcker/SO-Mod-UserScripts/raw/master/ModMessageHelper/dist/ModMessageHelper.user.js
+//
+${buildMatchPatterns('// @match        ', '/users/message/create/*')}
+//
+// @grant        none
+//
+// ==/UserScript==
+/* globals StackExchange, $ */`
+);
