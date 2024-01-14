de15169190d2a4b7e8c6f6c903630e25c2b9dcbc
HenryEcker <HenryEcker@users.noreply.github.com>
2024-01-14 09:40:50 -0500

    Use SVG for modal close button

--- MarkdownFromSuspicousVotesTable/MarkdownFromSuspicousVotesTable.user.ts
+++ MarkdownFromSuspicousVotesTable/MarkdownFromSuspicousVotesTable.user.ts
@@ -59,8 +59,8 @@ function main() {
             <div class="d-flex gx8 s-modal--footer">
                 <button class="flex--item s-btn" type="button" data-action="s-modal#hide">Close</button>
             </div>
-            <button class="s-modal--close s-btn s-btn__muted" type="button" data-action="s-modal#hide">
-                X
+            <button class="s-modal--close s-btn s-btn__muted" type="button" aria-label="Close" data-action="s-modal#hide">
+                <svg aria-hidden="true" class="svg-icon iconClearSm" width="14" height="14" viewBox="0 0 14 14"><path d="M12 3.41 10.59 2 7 5.59 3.41 2 2 3.41 5.59 7 2 10.59 3.41 12 7 8.41 10.59 12 12 10.59 8.41 7 12 3.41Z"></path></svg>
             </button>
         </div>
     </aside>`);
