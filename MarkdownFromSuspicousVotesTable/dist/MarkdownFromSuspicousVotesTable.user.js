18309ebd0987bf7371fc4e920590f7455a29e3f6
HenryEcker <HenryEcker@users.noreply.github.com>
2024-01-14 10:49:44 -0600

    Bump patch; rebuild

--- MarkdownFromSuspicousVotesTable/dist/MarkdownFromSuspicousVotesTable.user.js
+++ MarkdownFromSuspicousVotesTable/dist/MarkdownFromSuspicousVotesTable.user.js
@@ -3,7 +3,7 @@
 // @description  Creates an easy way to build a markdown table from a suspicious votes query
 // @homepage     https://github.com/HenryEcker-SO-UserScripts/SO-Mod-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.1
+// @version      0.0.2
 // @downloadURL  https://github.com/HenryEcker-SO-UserScripts/SO-Mod-UserScripts/raw/master/MarkdownFromSuspicousVotesTable/dist/MarkdownFromSuspicousVotesTable.user.js
 // @updateURL    https://github.com/HenryEcker-SO-UserScripts/SO-Mod-UserScripts/raw/master/MarkdownFromSuspicousVotesTable/dist/MarkdownFromSuspicousVotesTable.user.js
 //
@@ -21,32 +21,43 @@
 /* globals StackExchange, $ */
 (function() {
   "use strict";
+  function createLinkMd(text, href) {
+    return `[${text.replace("[", "\\[").replace("]", "\\]")}](${href})`;
+  }
   function processUserCardTd($td) {
     const userDisplayName = getTextFromJQueryElem($td.find(".user-details .s-btn"));
     const userLink = $td.find('a.s-block-link[href^="/users"]').attr("href");
-    return `[${userDisplayName}](${userLink})`;
+    return createLinkMd(userDisplayName, userLink);
   }
   function getTextFromJQueryElem($e) {
     return $e.text().trim();
   }
-  function* getTableRowElements($table) {
-    for (const n of $table.find("tbody tr")) {
+  function getTableRowElements($table) {
+    return $table.find("tbody tr").map((_0, n) => {
       const $e = $(n);
-      yield {
-        voter: processUserCardTd($e.find("td:eq(0)")),
-        targetUser: processUserCardTd($e.find("td:eq(1)")),
-        votesGiven: getTextFromJQueryElem($e.find("td:eq(2)")),
-        fraudSignal: getTextFromJQueryElem($e.find("td:eq(5)"))
+      return {
+        "Voter": processUserCardTd($e.find("td:eq(0)")),
+        "Target User": processUserCardTd($e.find("td:eq(1)")),
+        "Votes Given": getTextFromJQueryElem($e.find("td:eq(2)")),
+        "Fraud Signal": getTextFromJQueryElem($e.find("td:eq(5)"))
       };
-    }
+    }).toArray();
   }
   function buildTableMarkdown() {
     const $votesTable = $("table");
-    const markdown = ["| Voter | Target User | Votes Given | Fraud Signal |"];
-    markdown.push("|:---|:---|:---|:---|");
-    for (const { voter, targetUser, votesGiven, fraudSignal } of getTableRowElements($votesTable)) {
-      markdown.push(`| ${voter} | ${targetUser} | ${votesGiven} | ${fraudSignal} |`);
+    function makeMdRow(trData, withWhitespace = true) {
+      if (withWhitespace) {
+        return `| ${trData.join(" | ")} |`;
+      } else {
+        return `|${trData.join("|")}|`;
+      }
     }
+    const headers = ["Voter", "Target User", "Votes Given", "Fraud Signal"];
+    const markdown = [
+      makeMdRow(headers),
+      makeMdRow(headers.map((_) => ":---"), false),
+      ...getTableRowElements($votesTable).map((tbodyRow) => makeMdRow(headers.map((v) => tbodyRow[v])))
+    ];
     return {
       rows: markdown.length + 4,
       markdown: `
@@ -63,20 +74,20 @@ ${markdown.join("\n")}
     $btn.on("click", () => {
       const { rows, markdown } = buildTableMarkdown();
       const $textarea = $(`#${modalId}-textarea`);
-      $textarea.addClass("w100");
-      $textarea.css("max-height", "65vh");
       $textarea.attr("rows", rows);
       $textarea.val(markdown);
     });
     const $modal = $(`<aside class="s-modal" data-s-modal-target="modal" id="${modalId}" tabindex="-1" role="dialog" aria-labelledby="${modalId}-title" aria-describedby="${modalId}-description" aria-hidden="true">
         <div class="s-modal--dialog w70" role="document">
             <h1 class="s-modal--header" id="${modalId}-title">Table Markdown</h1>
-            <p class="s-modal--body" id="${modalId}-description"><textarea id="${modalId}-textarea"></textarea></p>
+            <p class="s-modal--body" id="${modalId}-description">
+                <textarea id="${modalId}-textarea" class="w100" style="max-height: 65vh;"></textarea>
+            </p>
             <div class="d-flex gx8 s-modal--footer">
                 <button class="flex--item s-btn" type="button" data-action="s-modal#hide">Close</button>
             </div>
-            <button class="s-modal--close s-btn s-btn__muted" type="button" data-action="s-modal#hide">
-                X
+            <button class="s-modal--close s-btn s-btn__muted" type="button" aria-label="Close" data-action="s-modal#hide">
+                <svg aria-hidden="true" class="svg-icon iconClearSm" width="14" height="14" viewBox="0 0 14 14"><path d="M12 3.41 10.59 2 7 5.59 3.41 2 2 3.41 5.59 7 2 10.59 3.41 12 7 8.41 10.59 12 12 10.59 8.41 7 12 3.41Z"></path></svg>
             </button>
         </div>
     </aside>`);
