dce7333e7378f3b6089520c8b6a20d18e54057d8
HenryEcker <HenryEcker@users.noreply.github.com>
2024-03-06 15:19:11 -0800

    Bump patch and rebuild

--- ModMessageHelper/vite.config.js
+++ ModMessageHelper/vite.config.js
@@ -9,7 +9,7 @@ export default buildViteConfig(
 // @description  Adds mod message templates with default configurations to the mod message drop-down
 // @homepage     https://github.com/HenryEcker-SO-UserScripts/SO-Mod-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.13
+// @version      0.0.14
 // @downloadURL  https://github.com/HenryEcker-SO-UserScripts/SO-Mod-UserScripts/raw/master/ModMessageHelper/dist/ModMessageHelper.user.js
 //
 ${buildMatchPatterns('// @match        ', '/users/message/create/*')}
