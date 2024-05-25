3b359d89814805f7fb2dc3866f5322204e8d39f9
Henry Ecker <HenryEcker@users.noreply.github.com>
2024-05-25 16:14:01 -0400

    Reorder Message Templates

--- ModMessageHelper/ModMessageHelper.user.ts
+++ ModMessageHelper/ModMessageHelper.user.ts
@@ -58,25 +58,6 @@ StackExchange.ready(function () {
 
     const customModMessages: UserDefinedMessageTemplate[] = [
         {
-            AnalogousSystemReasonId: 'SelfDestructionOfUsefulContent',
-            TemplateName: 'Vandalism (OpenAI protest)',
-            DefaultSuspendDays: 30,
-            IncludeSuspensionFooter: true,
-            TemplateBody: `It has come to our attention that you've removed or otherwise defaced some of your content in a manner that appears related to the recent announcement of Stack Exchange's partnership with OpenAI.
-
-We, the [volunteer community moderators](${parentUrl}/help/site-moderators), believe this is misguided. Our assumption is that Stack Overflow's content has long since been fed into many AI large language models (LLMs) already, [probably including the model powering ChatGPT](https://ai.stackexchange.com/a/38663), and nothing you or we do (or don't do) will change that.
-
-Thus, by destroying the copy that exists here, you are primarily harming two groups of people: 1) people who need answers to questions, search Stack Overflow for them, and find your posts, and 2) the volunteer moderators and curators who work to preserve Stack Overflow as a resource for others in the community.  LLM users will continue to be able to access answers regardless.
-
-We want Stack Overflow to remain a useful source of information for people who want vetted answers from humans. You may be assured that this is something we are willing to defend. When company policy threatened this, [we went on strike](https://meta.stackexchange.com/q/389811).
-
-While we are working to get further clarity and better public messaging, it is our current understanding (based on [this official statement from Stack Exchange](https://meta.stackexchange.com/a/399630)) that these agreements are a way to work with companies using Stack Exchange data to provide appropriate attribution (as per [the CC by-SA license](${parentUrl}/help/licensing)), rather than simply providing answers without attribution, as many do today.
-
-If you nonetheless wish to delete your account, please see [How do I delete my account?](https://meta.stackexchange.com/help/deleting-account). For requests regarding your personal data, use the [Stack Exchange Privacy Webform](https://policies.stackoverflow.co/data-request/); however, note that questions, answers, comments and other non-personally identifiable content posted on the site are not, generally, covered by privacy regulations like GDPR.  For general questions for staff, you can use the [contact form](${parentUrl}/contact).
-
-If there is something we've misunderstood about your actions, please let us know by replying to this message on the website.`
-        },
-        {
             AnalogousSystemReasonId: 'LowQualityQuestions',
             TemplateName: 'low quality answers',
             DefaultSuspendDays: 0,
@@ -215,6 +196,25 @@ As a result we have reset your username to the default setting. We kindly reques
 If there has been any misunderstanding regarding the meaning of the username you used, please feel free to reach out to us and provide clarification by responding to this message. Additionally, if you would like to change your username to something else that is appropriate and are experiencing any issues in doing so, please let us know and we will be happy to assist.
 
 Thank you for your understanding and cooperation.`,
+        },
+        {
+            AnalogousSystemReasonId: 'SelfDestructionOfUsefulContent',
+            TemplateName: 'Vandalism (OpenAI protest)',
+            DefaultSuspendDays: 7,
+            IncludeSuspensionFooter: true,
+            TemplateBody: `It has come to our attention that you've removed or otherwise defaced some of your content in a manner that appears related to the recent announcement of Stack Exchange's partnership with OpenAI.
+
+We, the [volunteer community moderators](${parentUrl}/help/site-moderators), believe this is misguided. Our assumption is that Stack Overflow's content has long since been fed into many AI large language models (LLMs) already, [probably including the model powering ChatGPT](https://ai.stackexchange.com/a/38663), and nothing you or we do (or don't do) will change that.
+
+Thus, by destroying the copy that exists here, you are primarily harming two groups of people: 1) people who need answers to questions, search Stack Overflow for them, and find your posts, and 2) the volunteer moderators and curators who work to preserve Stack Overflow as a resource for others in the community.  LLM users will continue to be able to access answers regardless.
+
+We want Stack Overflow to remain a useful source of information for people who want vetted answers from humans. You may be assured that this is something we are willing to defend. When company policy threatened this, [we went on strike](https://meta.stackexchange.com/q/389811).
+
+While we are working to get further clarity and better public messaging, it is our current understanding (based on [this official statement from Stack Exchange](https://meta.stackexchange.com/a/399630)) that these agreements are a way to work with companies using Stack Exchange data to provide appropriate attribution (as per [the CC by-SA license](${parentUrl}/help/licensing)), rather than simply providing answers without attribution, as many do today.
+
+If you nonetheless wish to delete your account, please see [How do I delete my account?](https://meta.stackexchange.com/help/deleting-account). For requests regarding your personal data, use the [Stack Exchange Privacy Webform](https://policies.stackoverflow.co/data-request/); however, note that questions, answers, comments and other non-personally identifiable content posted on the site are not, generally, covered by privacy regulations like GDPR.  For general questions for staff, you can use the [contact form](${parentUrl}/contact).
+
+If there is something we've misunderstood about your actions, please let us know by replying to this message on the website.`
         },
         {
             AnalogousSystemReasonId: 'OtherViolation',
